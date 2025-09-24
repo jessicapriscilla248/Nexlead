6a4260accd3d15668ede48819bdcb1c5cc7801d7
Jessica <153070653+jessicapriscilla248@users.noreply.github.com>
2025-09-24 20:30:04 +0700

    Update Results.py

--- pages/Results.py
+++ pages/Results.py
@@ -8,7 +8,7 @@ with st.sidebar:
         st.session_state.single_prediction_result = None
         st.session_state.csv_prediction_df = None
         st.session_state.page = 'Input Data'
-        st.switch_page("pages/📋_Input_Data.py")
+        st.switch_page("pages/Input_Data.py")
         st.rerun()
     
     st.markdown("---") 
@@ -25,7 +25,7 @@ if 'csv_view_index' not in st.session_state:
 
 st.title("Employee Promotion Prediction Results")
 
-# Kalau user input data dengan manual 
+# User input data dengan manual 
 if st.session_state.single_prediction_result:
     tab1, tab2 = st.tabs(['Prediction', 'Recommendation'])
 
@@ -66,7 +66,7 @@ if st.session_state.single_prediction_result:
         st.subheader("Processed Data Used for Prediction")
         st.dataframe(processed_data)
 
-# Kalau user input data pakai CSV
+# User input data pakai CSV
 if st.session_state.csv_prediction_df is not None:
     st.subheader("CSV Prediction Results")
     fileCSV = st.session_state.csv_prediction_df
@@ -76,7 +76,7 @@ if st.session_state.csv_prediction_df is not None:
     st.write(f"RECOMMENDED for Promotion     : {len(fileCSV[fileCSV['prediction'] == 1])}")
     st.write(f"NOT RECOMMENDED for Promotion : {len(fileCSV[fileCSV['prediction'] == 0])}")
     
-    # Detailed employee
+    # Detailnya employee
     st.subheader("Detailed Employee")
     if 'csv_view_index' not in st.session_state:
         st.session_state.csv_view_index = 0
@@ -97,7 +97,7 @@ if st.session_state.csv_prediction_df is not None:
     else:
         st.warning(f"{employee['prediction_label']} (Reliability: {employee['probability_not_promoted']*100:.1f}%)")
 
-    # Navigasi untuk back dan next melihat employee detail
+    # Back dan next cek employee detail
     col1, col2, col3 = st.columns([2, 2, 5])
     with col1:
         if st.button("⬅️ Previous") and current_index > 0:
@@ -122,4 +122,4 @@ if st.session_state.csv_prediction_df is not None:
 # Kondisi belum input data
 if st.session_state.single_prediction_result is None and st.session_state.csv_prediction_df is None:
     st.info("No predictions have been made yet. Please go to the 'Input Data' section.")
-    st.page_link("pages/📋_Input_Data.py", label="Go to Input Data →")
+    st.page_link("pages/Input_Data.py", label="Go to Input Data →")
